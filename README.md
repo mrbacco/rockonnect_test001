bf9e1d28c327eff23eb7b58fb0443d981ed6e3fd
Andrea Baccolini <andrea.baccolini@anam.com>
2019-07-17 14:15:49 +0100

    upload for threads

---
+++ README.md
@@ -0,0 +1,4 @@
+# rockonnect
+final web app project
+
+National COllege of Ireland: Higher Diploma in Science and Web Technologies. rockonnect web applciation
---
+++ models/threads.js
@@ -0,0 +1,21 @@
+
+var mongoose = require("mongoose");
+
+// needs to give some structured for the non structured db like mongodb
+var threadSchema = mongoose.Schema({
+    user:{
+        type: String,
+        required: false
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    content:{
+        type: String,
+        required: true
+    },
+});
+
+//export the db schema to be used in the threads creation etc ...
+var Threads = module.exports = mongoose.model("Thread", threadSchema); 
